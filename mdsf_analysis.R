0cc544145a8edb260a942b5e7860adc418b42b09
victoriadinzeo <s2744812@ed.ac.uk>
2026-06-07 19:06:12 +0200

    Update mdsf_analysis.R

--- mdsf_analysis.R
+++ mdsf_analysis.R
@@ -7,6 +7,17 @@ library(janitor)
 data_path <- './data/mdsf_2022-03-31.xlsx'
 
 # read-data
-mdsf_data <- read_xlsx(file_path, col_types = 'text')
+mdsf_data <- read_xlsx(data_path)
 
+# data wrangling
+mdsf_data_clean <- mdsf_data %>% 
+  # change columns that should be numeric to numeric
+  mutate(DateReg = as.numeric(DateReg),
+         SIMD_rank = as.numeric(SIMD_rank),
+         SIMD2020_Decile = as.numeric(SIMD2020_Decile)
+         ) %>% 
+  # turn excel number to date
+  mutate(DateReg = excel_numeric_to_date(DateReg, 
+                                         date_system = "modern",
+                                         include_time = FALSE))
 
